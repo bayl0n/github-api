4df8f7e03a108371de894f6d3e34fece16a3f095
Nathan <nathanbaylon@gmail.com>
2021-04-24 03:51:07 +1000

    Refined static remove button

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Typography, Card, CardContent, Link, CardHeader, CardMedia, CardActions, IconButton, Collapse } from '@material-ui/core';
+import { Box, Typography, Card, CardContent, Link, CardHeader, CardMedia, CardActions, IconButton, Collapse } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import RemoveCircleRoundedIcon from '@material-ui/icons/RemoveCircleRounded';
 
 function UserCard(props) {
 
@@ -13,34 +14,37 @@ function UserCard(props) {
     }
 
     return (
-        <Card align="left" style={{ position: "relative" }}>
-            <IconButton style={{ position: "absolute", top: "0", right: "0", backgroundColor: "rgba(255,255,255,1)", borderRadius: "1rem'" }}>
-                <DeleteForeverIcon htmlColor="rgb(200, 100, 100)" />
+        <Box style={{ position: "relative" }}>
+            <IconButton style={{ position: "absolute", top: "-1.5rem", right: "-1.5rem" }}>
+                <RemoveCircleRoundedIcon fontSize="default" htmlColor="rgb(200, 100, 100)" />
             </IconButton>
-            <Link href={props.htmlURL} target="_blank">
-                <CardMedia component="img" height="200" src={props.avatarURL} />
-            </Link>
-            <CardHeader title={props.login} subheader={props.name} />
-            {props.bio !== null ?
-                <>
-                    <CardActions>
-                        <IconButton onClick={handleClick}>
-                            <ExpandMoreIcon />
-                            <Typography variant="body1" color="textSecondary">More</Typography>
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={expanded} unmountOnExit>
-                        <CardContent>
-                            <Typography variant="h6" color="textSecondary">Bio</Typography>
-                            <Typography variant="body1" color="textPrimary">
-                                {props.bio}
-                            </Typography>
-                        </CardContent>
-                    </Collapse>
-                </>
-                : null
-            }
-        </Card>
+
+            <Card align="left">
+                <Link href={props.htmlURL} target="_blank">
+                    <CardMedia component="img" height="200" src={props.avatarURL} />
+                </Link>
+                <CardHeader title={props.login} subheader={props.name} />
+                {props.bio !== null ?
+                    <>
+                        <CardActions>
+                            <IconButton onClick={handleClick}>
+                                <ExpandMoreIcon />
+                                <Typography variant="body1" color="textSecondary">More</Typography>
+                            </IconButton>
+                        </CardActions>
+                        <Collapse in={expanded} unmountOnExit>
+                            <CardContent>
+                                <Typography variant="h6" color="textSecondary">Bio</Typography>
+                                <Typography variant="body1" color="textPrimary">
+                                    {props.bio}
+                                </Typography>
+                            </CardContent>
+                        </Collapse>
+                    </>
+                    : null
+                }
+            </Card>
+        </Box>
     )
 }
 
